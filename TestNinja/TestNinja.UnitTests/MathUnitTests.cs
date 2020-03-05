5490c43a08554a4d6e5c5e79a814bd3f51b71fb7
Gray, David <dgray2@nz.imshealth.com>
2020-03-05 17:25:34 +1300

    initial commit

---
+++ TestNinja/TestNinja.UnitTests/MathUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathUnitTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSumOfArguments()
+        {
+            //var math = new Math();
+            TestNinja.Fundamentals;
+        }
+    }
+}
---
+++ TestNinja/TestNinja/UnitTests/MathUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class MathUnitTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnSummedArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result.Equals(3));
+        }
+
+        [Test]
+        public void Max_ArgumentsDiffer_ReturnLargerArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result.Equals(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsSame_ReturnLargerArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result.Equals(1));
+        }
+    }
+}
